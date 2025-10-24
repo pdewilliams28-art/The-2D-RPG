5d7f348742f582d8c9333735dfc7529db3f4c6c1
pdewilliams28-art <pdewilliams28@bfhsla.org>
2025-10-24 15:22:04 -0500

    Textures

--- carl.gd
+++ carl.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var move_speed: float = 250.0
+@export var move_speed: float = 100.0
 
 
 
@@ -8,4 +8,15 @@ func _physics_process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = move_vector * move_speed
 	
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("walk_right")
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("walk_left")
+	elif velocity.y > 0:
+		$AnimatedSprite2D.play("walk_down")
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play("walk_up")
+	else:
+		$AnimatedSprite2D.stop	
+		
 	move_and_slide()
