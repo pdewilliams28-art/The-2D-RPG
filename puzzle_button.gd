9a67fd877bfb0c56532c60ad83054b34c4efe507
pdewilliams28-art <pdewilliams28@bfhsla.org>
2025-11-13 15:29:19 -0600

    doors and buttons

--- puzzle_button.gd
+++ puzzle_button.gd
@@ -1,6 +1,7 @@
 extends Area2D
-
-
+signal pressed
+signal unpressed
+var bodies_on_button: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,12 +14,20 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	print("aaahhh")
+	bodies_on_button += 1
+	if body.is_in_group("movable") or body is Player:
+		if bodies_on_button == 1:
 	#play animation
 	#open a door by sneding a signal
-	$AnimatedSprite2D.play("On")
+			$AnimatedSprite2D.play("On")
+			pressed.emit()
 
 func _on_body_exited(body: Node2D) -> void:
 	print("airrr")
-	$AnimatedSprite2D.play("Off")
+	bodies_on_button -= 1
+	if body.is_in_group("movable") or body is Player:
+		if bodies_on_button == 0:
+			$AnimatedSprite2D.play("Off")
 	#play off animation
-	#shut the door
+	#shut the door	
+			unpressed.emit()
